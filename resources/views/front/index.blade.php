bc3b21a226e895a82dd7ef9173ac4759f8acb16d
juarezfdo <42845913+juarezfdo@users.noreply.github.com>
2021-02-24 19:35:31 -0600

    Actualizacion de index

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -2,33 +2,44 @@
 
 @section('body')
 
-	<div style="background-color: #F0F0F1; padding-top: 80px;padding-bottom: 80px;">
+	<style>
+		html,body{
+			height: 100%;
+		}
+		.contenedor{
+			height: 100%;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
+
+	</style>
+
+	<div class="contenedor justify-content-center align-self-center" style="background-color: #F0F0F1;">
 		<div class="container">
-			<div class="row justify-content-between">
-				<div class="col-lg-6" style="padding-bottom: 20px;">
-					<div class="encabezado">
-						<h1>Todo lo</h1>
-						<h1 style="margin-top: -15px;">que sientes</h1>
-						<h1 style="color: #F5B530;margin-top: -15px;">es real.</h1>
-					</div>
+			<div class="row">
+				<div class="col-lg-6">
+					<h1>Todo lo</h1>
+					<h1 style="margin-top: -15px;">que sientes</h1>
+					<h1 style="color: #F5B530;margin-top: -15px;">es real.</h1>
 					<p><b>Es muy importante tratar la salud mental. En mente 4.0 tenemos un plan que se acomoda a tus necesidades, con profesionales que resolverán todas tus dudas, todo desde la comodidad de tu hogar.</b></p>
 					<p>Todos nosotros, en determinados momentos de nuestras vidas, necesitamos tener asesoramiento y recibir la ayuda de otras personas.</p>
-					<a class="btn" href="#" style="background-color: #F47E6E;color:white;" role="button">Conoce Más</a>
+					<a class="btn" href="#" style="background-color: #F47E6E;color:white;" role="button">Comienza ahora</a>
 				</div>
 				<div class="col-lg-6">
 					<img src="images/img1.png" class="img-fluid">
 				</div>
-			</div>	
-		</div>	
+			</div>
+		</div>
 	</div>
 
-	<div style="padding-top: 80px;padding-bottom: 80px;">
+	<div class="contenedor">
 		<div class="container">
 			<div class="row justify-content-between">
 				<div class="col-lg-6">
 					<img src="images/img2.png" class="img-fluid">
 				</div>
-				<div class="col-lg-6" style="padding-bottom: 20px;">
+				<div class="col-lg-6 justify-content-center align-self-center" style="padding-bottom: 20px;">
 					<div class="encabezado">
 						<h1>Tu tienes el poder</h1>
 						<h1 style="margin-top: -15px;">en la terapia</h1>
@@ -39,10 +50,10 @@
 					<h5 style="text-align: right; color: #F5B530;"><b>Piensa, Experimenta y Aprende.</b></h5>
 				</div>
 			</div>	
-		</div>	
+		</div>
 	</div>
 
-	<div style="background-color: #F0F0F1; padding-top: 80px;padding-bottom: 80px;">
+	<div class="contenedor" style="background-color: #F0F0F1;">
 		<div class="container">
 			<h1 style="text-align: center;color: #77D0E8">
 				Testimonios
@@ -52,26 +63,21 @@
 			<br>
 			<div class="row justify-content-between">
 				<div class="col-lg-3" style="background-color: white;padding: 20px;">
-					<h5 style="text-align: center">Eugenia Salazar</h5>
+					<h5 style="text-align: center">Alesya, 25 años</h5>
 					<p style="text-align: center;"><i style="color:#27CC9A" class="fa fa-2x fa-quote-left"> </i></p>
-					<p style="text-align: center"><i>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua."</i></p>
+					<p style="text-align: center"><i>"La terapia me ha devuelto la capacidad de continuar con mi vida."</i></p>
 				</div>
 				<div class="col-lg-3" style="background-color: white;padding: 20px;">
-					<h5 style="text-align: center">Eugenia Salazar</h5>
+					<h5 style="text-align: center">Javier, 38 años</h5>
 					<p style="text-align: center;"><i style="color:#27CC9A" class="fa fa-2x fa-quote-left"> </i></p>
-					<p style="text-align: center"><i>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua."</i></p>
+					<p style="text-align: center"><i>"Ahora puedo identificar tipos de pensamientos que no me ayudaban, a vivir menos angustiado, más tranquilo, a canalizar emociones, a conocerme mejor a mí mismo."</i></p>
 				</div>
 				<div class="col-lg-3" style="background-color: white;padding: 20px;">
-					<h5 style="text-align: center">Eugenia Salazar</h5>
+					<h5 style="text-align: center">Eva, 33 años</h5>
 					<p style="text-align: center;"><i style="color:#27CC9A" class="fa fa-2x fa-quote-left"> </i></p>
-					<p style="text-align: center"><i>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua."</i></p>
+					<p style="text-align: center"><i>"Empecé a disfrutar de la vida sin pareja y darme cuenta de que no necesitaba a nadie para estar completa."</i></p>
 				</div>
 			</div>	
-		</div>	
+		</div>
 	</div>
-	
-
 @endsection
