af49ca082c32d05d5d67084da64c55118cdd79ce
juarezfdo <juarezfdo96@gmail.com>
2021-03-02 02:58:06 -0600

    header

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,52 +1,34 @@
 <template>
   <div>
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-lg-4">
-          <img src="/images/logo.svg" class="img-fluid" style="padding: 20px" />
-        </div>
+    <!-- Comienza header -->
+  <nav class="navbar navbar-expand-lg navbar-light" style="background: rgba(255, 255, 255, 0); border-color: black;">
+    <div class="container" >
+      <a class="navbar-brand" href="/home"><img src="/images/logo.svg" width="100%" class="img-fluid d-inline-block align-top"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"style="color:black;" ></span>
+  </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link vinculos" href="/">Psicologos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link vinculos" href="/perfiles">Planes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link vinculos" href="/registro">Contacto</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link vinculos" href="/contacto">Registro</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link vinculos" href="/contacto">Iniciar Sesión</a>
+          </li>
+        </ul>
       </div>
     </div>
-    <nav class="navbar navbar-expand-lg navbar-light"
-      style="background-color: #27CC9A">
-      <div class="container">
-        <a class="navbar-brand" href="#"></a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div
-          class="collapse navbar-collapse justify-content-between"
-          id="navbarSupportedContent"
-        >
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link menu" href="/">Inicio</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link menu" href="/perfiles">Perfiles</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link menu" href="/planes">Planes</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link menu" href="/contacto/crear">Contacto</a>
-            </li>
-          </ul>
-          <ul class="navbar-nav">
-            <li><a  href="/Registro" class="nav-link">Registro</a></li>
-            <li><a  href="/Login" class="nav-link">Iniciar Sesión</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+  </nav>
+  <!-- Termina Header header -->
   </div>
 </template>
 
